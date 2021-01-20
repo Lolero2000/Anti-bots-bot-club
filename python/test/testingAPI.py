6347a48e568bdd0446c1fc51b0a1694ef11572b8
Javier Barceló Pérez <javibarpe@hotmail.com>
2021-01-20 18:41:35 +0100

    removed private keys from files, now saved in external file

--- python/test/testingAPI.py
+++ python/test/testingAPI.py
@@ -1,16 +1,13 @@
-# API KEY = b659JiTJbR2zG0zh0oOv06Mhu
-# API SECRET KEY = A97rWT7WkGu9bGZJkO0T28E5XJOYs1R36Yzr4nJVNCDihwUi7G
-# BEARER TOKEN = AAAAAAAAAAAAAAAAAAAAANHTLwEAAAAA%2FxQsn%2BCfGYV8Ld1Dyx2UXiaZ6NI%3DOBdx42UBDU9ThF6CPfUY701ojlgFPX4ZnOIwVEhPVV321nGIuZ
-# ACCESS TOKEN = 721099414-bqBLJ2TDXvKN61Ct5nwLeAqkEe9zIaU1ueLbr6u1 
-# ACCESS TOKEN SECRET = Qfh78eUs6XrxwKROxxnqOsNrfiJhU55HB3PGXYLIC8b29
-
 import tweepy
 
-if __name__ == '__main__':   
-    API_key = "b659JiTJbR2zG0zh0oOv06Mhu"
-    API_secret_key = "A97rWT7WkGu9bGZJkO0T28E5XJOYs1R36Yzr4nJVNCDihwUi7G"
-    access_token = "721099414-i0lDyCg4rUWcOKrPkJGLiH513yDMCjkWVa02ehFh"
-    access_token_secret = "6ZDhWZBhO6VXbXqBVB5sIBPAZevke0wgU5tEANFjWa8m9"
+if __name__ == '__main__':
+    
+    with open('keys.txt') as f:
+        creds = [line.rstrip() for line in f]
+    API_key = creds[0]
+    API_secret_key = creds[1]
+    access_token = creds[2]
+    access_token_secret = creds[3]
     
     auth = tweepy.OAuthHandler(API_key, API_secret_key)
     auth.set_access_token(access_token, access_token_secret)
